3124f5a728074db70a6c3fdeaec4cfb384035cd9
Nevin <nevinm2021@gmail.com>
2025-04-14 00:22:35 +0530

    env db.js added

--- backend/blog-engine/config/middlewares.ts
+++ backend/blog-engine/config/middlewares.ts
@@ -2,15 +2,7 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: ["https://your-frontend-domain.com"], // Add your production frontend domain here
-      headers: ["Content-Type", "Authorization"], // Customize headers if needed
-      methods: ["GET", "POST", "PUT", "DELETE"], // Customize methods if needed
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
